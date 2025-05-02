22f956247c43b55a7d4c79f62acd629d0e65910b
Rahulm9508 <153660317+Rahulm9508@users.noreply.github.com>
2025-05-02 21:59:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "29403305"
-api_hash = "54c435573d0f2d9dada270a0cb493b36"
-bot_token = "7559952755:AAH-zCMc9N4VhvB1K69rgFfcY5X-VCi8s0Y"
-auth_users = [6381511858]
+api_id = "20346550"
+api_hash = "bc79c3bea7a626887bdc0871eecf0327"
+bot_token = "7358697038:AAGlf2X62lm8Wg2L-XUb7zQILyq3nN8v5qM"
+auth_users = [7081036509]

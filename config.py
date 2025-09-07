b6139986602305a5ee3e3e97d3576435898638e3
Rahulm9508 <153660317+Rahulm9508@users.noreply.github.com>
2025-09-07 17:00:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 api_id = "20346550"
 api_hash = "bc79c3bea7a626887bdc0871eecf0327"
-bot_token = "7358697038:AAGlf2X62lm8Wg2L-XUb7zQILyq3nN8v5qM"
+bot_token = "7358697038:AAHJJrrhE8hC6zjlUbRJorat9LFwDGehdAc"
 auth_users = [7081036509]

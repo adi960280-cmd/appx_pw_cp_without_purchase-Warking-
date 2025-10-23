872791327cb9293417d151ed1477cf52f176d65e
adi960280-cmd <adityamalhotra960@gmail.com>
2025-10-23 21:52:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "20346550"
-api_hash = "bc79c3bea7a626887bdc0871eecf0327"
-bot_token = "7358697038:AAHJJrrhE8hC6zjlUbRJorat9LFwDGehdAc"
-auth_users = [7081036509]
+api_id = "29115102"
+api_hash = "1a331db2b00e9d2decaa9c7276449eb6"
+bot_token = "8316278584:AAE_hOihEQ0DjFCr1tbG4Eet-6c3qkyLotM"
+auth_users = [1224092270]
